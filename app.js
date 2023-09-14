2c3e8d8c6af68e42154287d85b0f6713d6fb3feb
hectorln <hector.lucero193@tectijuana.edu.mx>
2023-09-13 22:38:31 -0700

    Endpoints testing

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
 
-app.use((req, res, next) =>{    //Sets up middleware
-    res.status(200).json({      //Sends a response
-        message: 'It works!'
-    });
-});
+app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
+// app.use((req, res, next) =>{    //Sets up middleware
+//     res.status(200).json({      //Sends a response
+//         message: 'It works!'
+//     });
+// });
 
 module.exports = app;
