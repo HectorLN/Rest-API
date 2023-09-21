f694e0d20e5200f3ecba6109717de7e379e641c4
HectorLN <hector.lucero193@tectijuana.edu.mx>
2023-09-20 18:51:30 -0700

    Added body parser and CORS handling

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
-const morgan = require('morgan');
 
+//Middlewares
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//CORS handling
+app.use((req, res, next) => {
+    res.header('Acess-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+//End points
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
-// app.use((req, res, next) =>{    //Sets up middleware
-//     res.status(200).json({      //Sends a response
-//         message: 'It works!'
-//     });
-// });
 
+//Error handling
 app.use((req, res, next) =>{
     const error = new Error('Not found');
     error.status = 404;
